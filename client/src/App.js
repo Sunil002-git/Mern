d2dccef9b8d9108735d5bb161c1946ccf633893d
Sunil002-git <sunilmarri886@gmail.com>
2026-03-04 15:37:29 +0530

    delete done

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, {useState} from 'react';
+
+// function App(){
+
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <h1> Counter: {count}</h1>
+//       <button onClick={() => setCount(count + 1)}>Increase</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+import axios from 'axios';
+import React, {useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Fetch Users
+  const fetcUsers= () => {
+    // fetch("http://localhost:5000/users")
+    // .then((res) => res.json())
+    // .then((data) => setUsers(data))
+    axios.get("http://localhost:5000/users")
+    .then((res) => setUsers(res.data))
+    .catch((err) => console.log(err));
+  }
+
+  useEffect(() => {
+    fetcUsers();
+  }, []);
+
+// Create User
+// const createUser = () => {
+//   fetch("http://localhost:5000/create-user", {
+//     method: "POST",
+//     headers : {
+//       "Content-Type" : "application/json",
+//     },
+//     body: JSON.stringify({name, email, password}),
+//     // This converts a JavaScript object into JSON string.
+//   })
+//   .then((res) => res.json())
+//   .then(() => {
+//     setName("");
+//     setEmail("");
+//     setPassword("");
+//     fetcUsers();
+//   });
+// };
+const createUser = () => {
+  axios.post("http://localhost:5000/create-user", {
+    name, email, password
+  }) .then(() => {
+    setName("");
+    setEmail("");
+    setPassword("");
+    fetchUsers();
+  }).catch((err) => console.log(err));
+};
+
+// const deleteUser = (id) => {
+//   fetch(`http://localhost:5000/delete-user/${id}` , {
+//     method: "DELETE",
+//   }).then(() => fetcUsers());
+// };
+const deleteUser = (id) => {
+  axios.delete(`http://localhost:5000/delete-user/${id}`)
+  .then(() => fetchUsers())
+  .catch((err) => console.log(err));
+};
+
+return (
+  <div style= {{padding: "30px"}}>
+    
+  <h2>Create User</h2>
+  <input placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
+  <input placeholder='Email' value={email} onChange= {(e) => setEmail(e.target.value)} />
+  <input placeholder='Password' value={password} onChange= {(e) => setPassword(e.target.value)} />
+  <button onClick={createUser}>Add User</button>
+
+    <h2>Users List</h2>
+    {users.map((user) => (
+      <div key={user._id}>
+          <p>
+            {user.name} - {user.email} <button onClick={() => deleteUser(user._id)}>Delete</button>
+          </p>
+          
+        </div>
+    ))}
+  </div>
+)
+
 }
 
 export default App;
--- server.js
+++ server.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors');
 app.use(express.json());
+app.use(cors());
 
 // mongoose.connect("mongodb://127.0.0.1:27017/mernDB")
 // .then(() => console.log("MongoDB connetced."))
